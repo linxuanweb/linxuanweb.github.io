889fd9b05ed526966f51fe0f574932c9f8ddd10b
linxuan-li <linxuan-li@Linxuans-MacBook-Pro.local>
2023-03-28 18:26:16 +0800

    update

--- src/app/openai/openai.component.ts
+++ src/app/openai/openai.component.ts
@@ -13,26 +13,53 @@ export class OpenaiComponent implements OnInit {
 
   fetching = false;
 
+  ws: WebSocket;
+
   constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.connectToServer().then((ws: WebSocket) => {
+      this.ws = ws;
+      ws.onmessage = (e) => {
+        const parsedData = JSON.parse(e.data);
+        this.answer = parsedData.open_ai;
+        if (parsedData.is_end) {
+          this.fetching = false;
+        }
+      };
+    });
+  }
+
+  connectToServer() {
+    const ws = new WebSocket('ws://8316-45-62-167-199.ngrok.io/ws/open_ai');
+    return new Promise((resolve, reject) => {
+      const timer = setInterval(() => {
+        if (ws.readyState === 1) {
+          clearInterval(timer);
+          resolve(ws);
+        }
+      }, 10);
+    });
+  }
 
   go() {
     if (this.prompt) {
       this.fetching = true;
-      this.http
-        .request('POST', 'https://8316-45-62-167-199.ngrok.io/open_ai/prompt', {
-          body: {
-            prompt: this.prompt,
-          },
-        })
-        .subscribe(
-          (data: any) => {
-            this.answer = data.open_ai;
-          },
-          () => {},
-          () => (this.fetching = false)
-        );
+      this.ws.send(JSON.stringify({ prompt: this.prompt }));
+      //   this.fetching = true;
+      //   this.http
+      //     .request('POST', 'https://8316-45-62-167-199.ngrok.io/open_ai/prompt', {
+      //       body: {
+      //         prompt: this.prompt,
+      //       },
+      //     })
+      //     .subscribe(
+      //       (data: any) => {
+      //         this.answer = data.open_ai;
+      //       },
+      //       () => {},
+      //       () => (this.fetching = false)
+      //     );
     }
   }
 }
